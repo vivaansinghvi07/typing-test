d62670dac4f3c453f8abed36fa61bfd9c96dc98b
vivaansinghvi07 <s945958@student.knoxschools.org>
2023-03-20 18:03:17 -0400

    finished settings methods

--- settings.py
+++ settings.py
@@ -23,15 +23,14 @@ def printIntro():
               "In this game, you will be given a prompt, which you will need to type out (accurately).",
               "While taking a test, you can press Tab to regenerate your test and Enter to quit.", 
               "Then, after you finish, you will be given your typing speed! There are various tests to choose from.",
-              "",
-              "Press enter when you are ready to begin."]
+              ""]
     
     # prints every line
     for line in prompt:
         print(line)
 
     # waits for enter press to begin
-    input()
+    input("Press enter when you are ready to begin. ")
 
 def getLengthSettings():
     # clears system
@@ -40,10 +39,13 @@ def getLengthSettings():
     # defines choice
     option = -1
 
+    # prints prompt
+    print("How many words would you like to type?")
+
     # gets option in range
-    while 0 < option and option < 200:
+    while not (0 < option and option < 200):
         try:
-            option = int(input("Enter a word count between 1 and 200: "))
+            option = int(input("Enter a number between 1 and 200: "))
         except:
             print("Please enter an integer!")
             continue
@@ -52,4 +54,30 @@ def getLengthSettings():
     # returns chosen number
     return option
 
-printIntro()
+def getModeSettings():
+    # clears system
+    system('clear')
+
+    # defines choice
+    option = ""
+
+    # prints prompt
+    prompt = ["What mode would you like to test on?",
+              "",
+              "  1. Sentences",
+              "  2. Easy Words", 
+              "  3. Hard Words",
+              ""]
+    for line in prompt:
+        print(line)
+    
+    # gets the option
+    while option not in ["1", "2", "3", "b"]:
+        option = input("Enter \"b\" to go back, or a number to choose a mode: ")
+
+    # returns the mode if possible, otherwise returns the exit code
+    try:
+        return ["essay", "easy", "hard"][int(option)]
+    except:
+        return -1
+    
