8d2c2c49b408bd4800d89b39f29b51533d6c2098
vivaansinghvi07 <s945958@student.knoxschools.org>
2023-03-20 10:33:00 -0400

    added method to get words

--- getdata.py
+++ getdata.py
@@ -1,4 +1,4 @@
-import random as rnd
+import random as rand
 
 def getRandomEssays(wordCount):
     # opens the file
@@ -8,7 +8,28 @@ def getRandomEssays(wordCount):
         lines = f.readlines()
 
         # gets a randomline
-        line = lines[rnd.randint(0, len(lines)-1)]
+        line = lines[rand.randint(0, len(lines)-1)]
 
     # returns a random line with a specified word count
     return " ".join(line.split(" ")[0:wordCount])
+
+def getWords(wordCount, difficulty):
+
+    # defines filename based on difficulty
+    filename = "1-1000" if difficulty == "easy" else "engmix"
+
+    # defines output
+    output = ""
+
+    # opens the file
+    with open(f"words/{filename}.txt", "r") as f:
+
+        # gets all the lines
+        lines = f.readlines()
+
+        # gets random lines
+        for _ in range(wordCount):
+            output += rand.randint(0, len(lines)-1)
+    
+    # return
+    return output
